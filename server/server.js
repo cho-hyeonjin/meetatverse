84b68dd77d2d8f562524d2b99edf4a859f69556e
cho-hyeonjin <gusls2005@gmail.com>
2024-04-28 00:45:55 +0900

    setup: Socket.io server✨

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-console.log("server/server.js 실행");
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost; 5173",
+  },
+});
+
+io.listen(3001);
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.emit("Hello~👋🏻");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
