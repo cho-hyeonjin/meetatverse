03cb28b28f677f1630a65c8fea94c68622511440
cho-hyeonjin <gusls2005@gmail.com>
2024-04-27 21:01:20 +0900

    feat: Replace temporary mesh with glb model(BusinessMan.jsx)

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,13 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
+import { BusinessMan } from "./BusinessMan";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <BusinessMan />
     </>
   );
 };
