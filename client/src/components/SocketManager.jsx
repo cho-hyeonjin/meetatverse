9c8cf5ae068029065e3c6c34a5218291408af561
cho-hyeonjin <gusls2005@gmail.com>
2024-04-28 00:46:22 +0900

    setup: Socket.io client✨

---
+++ client/src/components/SocketManager.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+export const socket = io("http://localhost:3001");
+
+export const SocketManager = () => {
+  useEffect(() => {
+    function onConnect() {
+      console.log("connected");
+    }
+
+    function onDisconnect() {
+      console.log("disconnected");
+    }
+
+    function onHello() {
+      console.log("Hello! 👋🏻");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("hello", onHello);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("hello", onHello);
+    };
+  }, []);
+};
