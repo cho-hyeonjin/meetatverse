0bf3900105d4911ce1892797268ac4d60c70c489
cho-hyeonjin <gusls2005@gmail.com>
2024-04-27 21:32:46 +0900

    Setup the Environment and Light & Basic Animation to the BusnessMan model✨

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,9 +1,11 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { BusinessMan } from "./BusinessMan";
 
 export const Experience = () => {
   return (
     <>
+      <Environment preset="sunset" />
+      <ambientLight intensity={0.3} />
       <OrbitControls />
       <BusinessMan />
     </>
