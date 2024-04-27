92e7007781d4d9adb07a2fa8ce1b2631a171cdef
cho-hyeonjin <gusls2005@gmail.com>
2024-04-28 00:28:17 +0900

    feat: Rendering Multiple Skinned Mesh models using React, R3f, drei, three-stdlib

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { BusinessMan } from "./BusinessMan";
+import { AnimatedWoman } from "./AnimatedWoman";
 
 export const Experience = () => {
   return (
@@ -7,7 +8,22 @@ export const Experience = () => {
       <Environment preset="sunset" />
       <ambientLight intensity={0.3} />
       <OrbitControls />
+      {/* <AnimatedWoman />
+      <AnimatedWoman
+        position-x={1}
+        hairColor="green"
+        topColor="tomato"
+        bottomColor="orangered"
+        feetColor="yellow"
+      /> */}
       <BusinessMan />
+      <BusinessMan
+        position-x={1}
+        hairColor="#545454"
+        topColor="#c3b091"
+        jacketColor="#2b2a29"
+        bottmColor="#171614"
+      />
     </>
   );
 };
