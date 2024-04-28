b421cbc6bbcbf54d0722400a4f66db8b07a81230
cho-hyeonjin <gusls2005@gmail.com>
2024-04-28 15:49:49 +0900

    ✨feat: add dictionary objects(interior item, map) & emit them

--- server/server.js
+++ server/server.js
@@ -11,9 +11,37 @@ io.listen(3001);
 /** 접속한 유저 정보 담는 Array - 누군가 로그인 할 때마다 아래 chracters 배열에 추가 */
 const characters = [];
 
+/** item dictionary */
+const items = {
+  table: {
+    name: "WoodTable",
+    size: [3, 6],
+  },
+  couch: {
+    name: "Couch",
+    size: [3, 3],
+  },
+  stepCubbyStorage: {
+    name: "StepCubbyStorage",
+    size: [5, 2],
+  },
+};
+
+/** map dictionary */
+const map = {
+  size: [10, 10],
+  gridDivision: 2,
+  items: [
+    {
+      ...items.couch,
+      gridPosition: [0, 0],
+    },
+  ],
+};
+
 /** 랜덤 포지션 생성 함수 - 누군가 접속할 때마다 위치할 포지션을 랜덤으로 부여하기 위함 */
 const generateRandomPosition = () => {
-  return [Math.random() * 3, 0, Math.random() * 3];
+  return [Math.random() * map.size[0], 0, Math.random() * map.size[1]];
 };
 /** 랜덤 hex 컬러 생성 함수 - 누군가 접속할 때마다 캐릭터 모델의 material 색상 조합을 랜덤으로 부여하기 위함 */
 const generateRandomHexColor = () => {
@@ -37,7 +65,12 @@ io.on("connection", (socket) => {
   });
 
   /** broadcating - 이벤트 발생시키는 부분 */
-  socket.emit("hello"); // 연결된 모든 client들에게 'hello'이벤트 발생
+  socket.emit("hello", {
+    map,
+    characters,
+    id: socket.id,
+    items,
+  }); // 연결된 모든 client들에게 'hello'이벤트 발생
 
   io.emit("characters", characters); // 연결된 모든 client들에게 'characters'이벤트 발생 - client에서 onCharacters 이벤트가 발생할 때마다 characters 배열에 client측 onCharacters의 value 파라미터에 들어온 새 접속자 정보를 server측 characters 배열에 추가
 
